bd1e2886d391384e0f48a002760fef0d173612c6
LandmineHQ <z1216198007@gmail.com>
2025-04-01 17:36:58 +0800

    feat(http): implement KubeJS HTTP plugin for making web requests
    - Rename project from KubeFilters to KubeJS HTTP (mkjs)
    - Create HttpKubeJS plugin with GET and POST request methods
    - Add support for custom headers and JSON data
    - Configure KubeJS plugin integration with required files
    - Disable Java null analysis in VSCode settings

---
+++ src/main/java/mortar/kubejs/mkjs/HttpKubeJS.java
@@ -0,0 +1,119 @@
+package mortar.kubejs.mkjs;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.latvian.mods.kubejs.KubeJSPlugin;
+import dev.latvian.mods.kubejs.script.BindingsEvent;
+
+public class HttpKubeJS extends KubeJSPlugin {
+    @Override
+    public void init() {
+        MkJSForge.LOGGER.info("Http Plugin初始化中...");
+    }
+
+    @Override
+    public void registerBindings(BindingsEvent event) {
+        event.add("HttpPlugin", new HttpPlugin());
+    }
+}
+
+class HttpPlugin {
+    /**
+     * 执行GET请求
+     * 
+     * @param urlString 请求URL
+     * @return 响应内容
+     */
+    public String get(String urlString) throws Exception {
+        return sendRequest(urlString, "GET", null, null);
+    }
+
+    /**
+     * 执行带有headers的GET请求
+     * 
+     * @param urlString 请求URL
+     * @param headers   请求头
+     * @return 响应内容
+     */
+    public String get(String urlString, Map<String, String> headers) throws Exception {
+        return sendRequest(urlString, "GET", headers, null);
+    }
+
+    /**
+     * 执行POST请求
+     * 
+     * @param urlString 请求URL
+     * @param jsonData  JSON格式数据
+     * @return 响应内容
+     */
+    public String post(String urlString, String jsonData) throws Exception {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        return sendRequest(urlString, "POST", headers, jsonData);
+    }
+
+    /**
+     * 执行带有headers的POST请求
+     * 
+     * @param urlString 请求URL
+     * @param headers   请求头
+     * @param jsonData  JSON格式数据
+     * @return 响应内容
+     */
+    public String post(String urlString, Map<String, String> headers, String jsonData) throws Exception {
+        if (headers == null) {
+            headers = new HashMap<>();
+        }
+        if (!headers.containsKey("Content-Type")) {
+            headers.put("Content-Type", "application/json");
+        }
+        return sendRequest(urlString, "POST", headers, jsonData);
+    }
+
+    /**
+     * 发送HTTP请求
+     */
+    private String sendRequest(String urlString, String method, Map<String, String> headers, String data)
+            throws Exception {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(method);
+
+        // 设置请求头
+        if (headers != null) {
+            for (Map.Entry<String, String> entry : headers.entrySet()) {
+                connection.setRequestProperty(entry.getKey(), entry.getValue());
+            }
+        }
+
+        // 发送数据
+        if (data != null && !data.isEmpty()) {
+            connection.setDoOutput(true);
+            try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                byte[] postData = data.getBytes(StandardCharsets.UTF_8);
+                outputStream.write(postData);
+                outputStream.flush();
+            }
+        }
+
+        // 获取响应
+        int responseCode = connection.getResponseCode();
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                responseCode >= 400 ? connection.getErrorStream() : connection.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        }
+        connection.disconnect();
+        return response.toString();
+    }
+}
---
+++ src/main/java/mortar/kubejs/mkjs/MkJSForge.java
@@ -0,0 +1,39 @@
+package mortar.kubejs.mkjs;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.event.server.ServerStoppedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(MkJSForge.MODID)
+public class MkJSForge {
+    public static final String MODID = "mkjs";
+    public static final Logger LOGGER = LogManager.getLogger(MkJSForge.MODID);
+
+    public MkJSForge() {
+        LOGGER.info(MkJSForge.MODID + "初始化中...");
+        MinecraftForge.EVENT_BUS.addListener(this::reloadListener);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onClientCommon);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onServerCommon);
+        MinecraftForge.EVENT_BUS.addListener(this::onServerEnding);
+    }
+
+    private void reloadListener(AddReloadListenerEvent event) {
+    }
+
+    private void onClientCommon(FMLClientSetupEvent event) {
+    }
+
+    private void onServerCommon(FMLDedicatedServerSetupEvent event) {
+        new HttpKubeJS();
+    }
+
+    private void onServerEnding(ServerStoppedEvent e) {
+    }
+}
